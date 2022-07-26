1e878ac506529a159bad1fba7fce995f189c57f0
patiberzin <pberzincand@gmail.com>
2022-07-26 16:57:01 -0300

    object

--- object/js/script.js
+++ object/js/script.js
@@ -1,105 +1,47 @@
-/*const pessoa = new Object({
-  nome: 'Pati'
-})
+//EXERCÍCIOS:
 
-console.log(pessoa)
+// Crie uma função que verifique
+// corretamente o tipo de dado
 
-const carro = {
-  marca: 'Marca',
-  rodas: 4,
-  init(valor) {
-    this.marca = 'valor';
-    return this;
-  },
-  acelerar() {
-    return this.marca + ' acelerou';
-  },
-  buzinar() {
-    return this.marca + ' buzinou';
-  }
+//toString
+
+function verificarDado(dado) {
+  return Object.prototype.toString.call(dado);
 }
 
-const honda = Object.create(carro).init('Honda');
+console.log(verificarDado({}));
 
-console.log(honda.acelerar());
+// Crie um objeto quadrado com
+// a propriedade lados e torne
+// ela imutável
 
-const ferrari = Object.create(carro).init('Ferrari');
+//criou uma propriedade imutável
+//quando ele colocou enumerable: true, ele a tornou imutável?
 
-console.log(ferrari.acelerar());*/
-
-/* Object.Assign() 
-
-const funcaoAutomovel = {
-  acelerar() {
-    return 'acelerou';
-  },
-  buzinar() {
-    return 'buzinou';
-  },
-}
-
-const moto = {
-  rodas: 2,
-  capacete: true,
-}
-
-Object.assign(moto, funcaoAutomovel); /* pode adicionar quantos objetos quiser 
-Ele substitui os valores pela cadeia de objetos que for colocando 
-
-console.log(moto);*/
-
-/* defineProperties()  */
-
-const moto = {
-  capacete: true,
-}
-
-
-Object.defineProperties(moto, { /* Passa um objeto com as propriedades */
-  rodas: {
+const quadrado = {};
+Object.defineProperties(quadrado, {
+  lados: {
+    value: 4,
     enumerable: true,
-    get() {
-      return 2; //propriedade q estou criando dentro do objeto.
-    },
-    set(valor) {
-      this._rodas = valor * 4;
-    }
   }
 })
 
+console.log(quadrado);
 
-console.log(moto);
-
-const innerHeightConfig = Object.getOwnPropertyDescriptor(window, 'innerHeight');
-
-Object.getOwnPropertyNames(Array.prototype);
-
-const frutas = ['Banana'];
-
-console.log(Object.getPrototypeOf(frutas));
-console.log(Array.prototype);
-
-const frutas1 = ['Banana', 'Pêra'];
-const frutas2 = ['Banana', 'Pêra'];
-
-const novaFruta = frutas1;
-
-novaFruta[0] = 'Uva'; //altera o primeiro valor da array
-
-console.log(frutas1); //retorna uva, pera
-
-console.log(Object.is(frutas1, novaFruta));
-
-const carro = {
-  marca: 'Ford',
-  ano: 2018,
+// Previna qualquer mudança
+// no objeto abaixo
+const configuracao = {
+  width: 800,
+  height: 600,
+  background: '#333'
 }
 
-Object.preventExtensions(carro);
-carro.portas = 4;
-delete carro.marca;
+Object.freeze(configuracao);
+//Se eu tentar adicionar qualquer coisa em configuração lá no console, ele não adiciona.
 
-carro.marca = 'Honda'
+// Liste o nome de todas
+// as propriedades do
+// protótipo de String e Array
 
-console.log(carro);
-console.log(Object.isSealed(carro));
+console.log(Object.getOwnPropertyNames(String.prototype))
+console.log(Object.getOwnPropertyNames(Array.prototype))
